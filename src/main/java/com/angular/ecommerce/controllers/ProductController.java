8f4f8cb96851267dbc0a936713fbafcaa60f5991
Mohamed Bhajy <mohamedbhajy23@gmail.com>
2021-05-30 23:56:50 +0100

    first commit

--- src/main/java/com/angular/ecommerce/controllers/ProductController.java
+++ src/main/java/com/angular/ecommerce/controllers/ProductController.java
@@ -2,15 +2,17 @@ package com.angular.ecommerce.controllers;
 
 import com.angular.ecommerce.dto.ProductDTO;
 import com.angular.ecommerce.services.IProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -25,5 +27,19 @@ public class ProductController {
         return ResponseEntity.ok()
                 .body(productService.getAllProducts(page));
 
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getBookById(@PathVariable("id") Long id){
+        return ResponseEntity.ok()
+                .body(productService.getProductById(id));
+    }
+    @PostMapping("/add")
+    public ResponseEntity<?> addBook(@RequestParam("images") List<MultipartFile> images,@RequestParam("product") String  productDTO) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProductDTO productDTO1 = objectMapper.readValue(productDTO,ProductDTO.class);
+        Boolean added = productService.addProduct(productDTO1,images);
+        if (added)
+        return ResponseEntity.ok().body("product added");
+        return ResponseEntity.badRequest().body("product Not added");
     }
 }
