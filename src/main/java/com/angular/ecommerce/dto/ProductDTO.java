82444aa971a77800fbdd02f05253e9a64f06212a
Mohamed Bhajy <mohamedbhajy23@gmail.com>
2021-06-01 00:37:18 +0100

    first commit

--- src/main/java/com/angular/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/angular/ecommerce/dto/ProductDTO.java
@@ -1,10 +1,12 @@
 package com.angular.ecommerce.dto;
 
+import com.angular.ecommerce.utils.Env;
 import lombok.Data;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class ProductDTO {
@@ -15,4 +17,8 @@ public class ProductDTO {
     private double price;
     private int stock;
     List<String> images = new ArrayList<String>();
+
+    public void setImages(List<String> images) {
+        this.images = images.stream().map(image -> Env.getUrlImages()+image).collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/com/angular/ecommerce/utils/Env.java
@@ -0,0 +1,18 @@
+package com.angular.ecommerce.utils;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Env {
+
+    public static String getUrlImages() {
+        try {
+            return "http://"+InetAddress.getLocalHost().getHostAddress()+":8080/api/images/";
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
